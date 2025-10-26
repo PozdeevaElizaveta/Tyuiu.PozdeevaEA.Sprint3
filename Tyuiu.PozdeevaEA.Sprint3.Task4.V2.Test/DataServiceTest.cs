48b0e5ae4532589e6d2b09feef3ad3a205e38d4a
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-10-26 21:31:38 +0500

    Task4

---
+++ Tyuiu.PozdeevaEA.Sprint3.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.PozdeevaEA.Sprint3.Task4.V2.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint3.Task4.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 0.104;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.PozdeevaEA.Sprint3.Task4.V2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.PozdeevaEA.Sprint3.Task4.V2.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Поздеева Е. А. | ПКТб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнил: Поздеева Елизавета Александровна | ПКТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+        Console.WriteLine("* функции y=cos(x)/x. При х = 0 прервать цикл. Полученные значения        *");
+        Console.WriteLine("* суммировать                                                             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine("Сумма ряда = " + ds.Calculate(startValue,stopValue));
+
+
+        Console.ReadKey();
+    }
+}
