c0769b4798c63ad51361a8f0beaaf49060f51080
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-10-26 23:32:20 +0500

    Task7

---
+++ Tyuiu.PozdeevaEA.Sprint3.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.PozdeevaEA.Sprint3.Task7.V12.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint3.Task7.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+
+            double[] valueWait;
+            valueWait = new double[len];
+            valueWait[0] = 6.49;
+            valueWait[1] = -9.88;
+            valueWait[2] = -12.26;
+            valueWait[3] = -4.60;
+            valueWait[4] = 0.52;
+            valueWait[5] = 4.00;
+            valueWait[6] = -0.75;
+            valueWait[7] = 4.55;
+            valueWait[8] = 12.23;
+            valueWait[9] = 9.86;
+            valueWait[10] = -6.50;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(res, valueWait);
+
+        }
+    }
+}
---
+++ Tyuiu.PozdeevaEA.Sprint3.Task7.V12/Program.cs
@@ -0,0 +1,61 @@
+﻿using Tyuiu.PozdeevaEA.Sprint3.Task7.V12.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Поздеева Е. А. | ПКТб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #12                                                             *");
+        Console.WriteLine("* Выполнил: Поздеева Елизавета Александровна | ПКТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая выводит таблицу значений функции:           *");
+        Console.WriteLine("*                    2                                                    *");
+        Console.WriteLine("* F(x) = sin(x) + ------ - 2 * cos(x) * 2 * x                             *");
+        Console.WriteLine("*                 3x+0.5                                                  *");
+        Console.WriteLine("* (произвести табулирование) f(x) на заданном диапазоне [-5; 5] с шагом 1.*");
+        Console.WriteLine("* Произвести проверку деления на ноль. При делении на ноль вернуть        *");
+        Console.WriteLine("* значение 0. Значения занести в массив. Значения округлить до двух       *");
+        Console.WriteLine("* знаков после запятой.                                                   *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+        double[] value;
+        value = new double[len];
+
+        value = ds.GetMassFunction(startValue,stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine("+------------+-------------+");
+        Console.WriteLine("|      x     |     f(x)    |");
+        Console.WriteLine("+------------+-------------+");
+        for (int i = 0; i <=len-1; i++)
+        {
+            Console.WriteLine("|{0,6:d}      |   {1,6:f2}    |", startValue, value[i]);
+            startValue++;
+        }
+        Console.WriteLine("+------------+-------------+");
+
+        Console.ReadKey();
+    }
+}
