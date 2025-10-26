c7e650bbd2345195ec27d2e1e57db66ecc5c7ab9
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-10-26 19:39:21 +0500

    Task2

---
+++ Tyuiu.PozdeevaEA.Sprint3.Task2.V9.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.PozdeevaEA.Sprint3.Task2.V9.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint3.Task2.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 10;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 0.042;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.PozdeevaEA.Sprint3.Task2.V9/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.PozdeevaEA.Sprint3.Task2.V9.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Поздеева Е. А. | ПКТб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #9                                                              *");
+        Console.WriteLine("* Выполнил: Поздеева Елизавета Александровна | ПКТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет сумму   *");
+        Console.WriteLine("* ряда по формуле, при n=5                                                *");
+        Console.WriteLine("*      10       1   2                                                     *");
+        Console.WriteLine("* S = summ  (------)                                                      *");
+        Console.WriteLine("*      k=1       k                                                        *");
+        Console.WriteLine("*              n                                                          *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int value = 5;
+        int startValue = 1;
+        int stopValue = 10;
+
+        Console.WriteLine("Переменная n = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+
+
+        Console.ReadKey();
+    }
+}
