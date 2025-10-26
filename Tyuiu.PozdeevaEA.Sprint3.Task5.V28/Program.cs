15566dbef4b71f6e496615642c3afaa49f7ec4ed
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-10-26 22:20:46 +0500

    Task5

---
+++ Tyuiu.PozdeevaEA.Sprint3.Task5.V28/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.PozdeevaEA.Sprint3.Task5.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнил: Поздеева Е. А. | ПКТб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла for                                                *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #28                                                              *");
+        Console.WriteLine("* Выполнил: Поздеева Елизавета Александровна | ПКТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                     x                                                   *");
+        Console.WriteLine("*      3    12       k                                                    *");
+        Console.WriteLine("* y = summ summ  (------)        при x = 2                                *");
+        Console.WriteLine("*      i=1  k=1   sin(k)                                                  *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 2;
+        int startValue1 = 1;
+        int stopValue1 = 3;
+        int startValue2 = 1;
+        int stopValue2 = 12;
+
+        Console.WriteLine("Переменная X = " + x);
+        Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+        Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+        Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+        Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine("Сумма сумм ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+
+        Console.ReadKey();
+    }
+}
